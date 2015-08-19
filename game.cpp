c7f734feb5abd792c2fde1856305f41f8afea3be
max-br <max.breitenfeldt@gmail.com>
2015-08-19 14:51:43 +0200

    remove assertions for performance testing with assertions disabled

--- game.cpp
+++ game.cpp
@@ -94,37 +94,55 @@ void runTests()
 	cout << "Test 1: " << endl;
 	board.setupFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); // starting position
 	board.printBoard();
-	assert(timePerft(board,movegen,6) == 119060324);
+	if(timePerft(board,movegen,6) != 119060324){
+		cout << "Test 1 fail" << endl;
+		return;
+	}
 	cout << "Test 1 success" << endl;
 
 	cout << "Test 2: " << endl;
 	board.setupFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 	board.printBoard();
-	assert(timePerft(board,movegen,5) == 193690690);
+	if(timePerft(board,movegen,5) != 193690690){
+		cout << "Test 2 fail" << endl;
+		return;
+	}
 	cout << "Test 2 success" << endl;
 
 	cout << "Test 3: " << endl;
 	board.setupFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
 	board.printBoard();
-	assert(timePerft(board,movegen,6) == 11030083);
+	if(timePerft(board,movegen,6) != 11030083){
+		cout << "Test 3 fail" << endl;
+		return;
+	}
 	cout << "Test 3 success" << endl;
 
 	cout << "Test 4: " << endl;
 	board.setupFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
 	board.printBoard();
-	assert(timePerft(board,movegen,5) == 15833292);
+	if(timePerft(board,movegen,5) != 15833292){
+		cout << "Test 4 fail" << endl;
+		return;
+	}
 	cout << "Test 4 success" << endl;
 
 	cout << "Test 5: " << endl;
 	board.setupFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
 	board.printBoard();
-	assert(timePerft(board,movegen,3) == 62379);
+	if(timePerft(board,movegen,3) != 62379){
+		cout << "Test 5 fail" << endl;
+		return;
+	}
 	cout << "Test 5 success" << endl;
 
 	cout << "Test 6: " << endl;
 	board.setupFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 	board.printBoard();
-	assert(timePerft(board,movegen,5) == 164075551);
+	if(timePerft(board,movegen,5) != 164075551){
+		cout << "Test 6 fail" << endl;
+		return;
+	}
 	cout << "Test 6 success" << endl;
 
 	cout << "All Tests successfull" << endl;
@@ -289,7 +307,6 @@ int main()
 	Movegen movegen;
 
 	runTests();
-
 
 	//startUci(board,movegen,eval);
 
